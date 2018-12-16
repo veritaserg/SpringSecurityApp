e2bd29e08d1038c491edc2c4a10d4b587c18ab4f
veritaserg <infophotomasterscomua@gmail.com>
2018-12-16 18:58:31 +0200

    update

--- src/main/java/springsecurityapp/service/UserService.java
+++ src/main/java/springsecurityapp/service/UserService.java
@@ -5,6 +5,5 @@ import springsecurityapp.model.User;
 public interface UserService {
 
     void save(User user);
-
     User findByUsername(String username);
 }
--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,3 +1,4 @@
+-- Table: users
 CREATE TABLE users (
   id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE user_roles (
 
 -- Insert data
 
-INSERT INTO users VALUES (1, 'proselyte', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
+INSERT INTO users VALUES (1, 'serg', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
 
 INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
